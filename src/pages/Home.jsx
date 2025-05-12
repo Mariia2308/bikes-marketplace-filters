08d4ce5d806b5ea54d566eebe7d96893fa77fdd0
Mariia Misskarisa <girlw4888@gmail.com>
2025-05-12 22:11:37 +0300

    homeloader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,35 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import BikesList from '../components/BikesList/BikesList';
 import PriceSlider from '../components/PriceSlider/PriceSlider';
-
-import { fetchBikes } from '../redux/bikes/bikeSlice';
 import Loader from '../components/Loader/Loader';
+import { fetchBikes } from '../redux/bikes/bikeSlice';
+import { selectLoading } from '../redux/bikes/bikeSelectors';
 
 function Home() {
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoading); // ✅ правильний селектор
 
   useEffect(() => {
     dispatch(fetchBikes());
   }, [dispatch]);
 
   return (
-    <div >
-      <div style={{ padding: '1rem 1.2rem ', borderBottom: '1px solid #ccc', paddingBottom: '1.2rem' }}>
+    <div>
+      <div style={{ padding: '1rem 1.2rem', borderBottom: '1px solid #ccc', paddingBottom: '1.2rem' }}>
         <h2>Filters</h2>
       </div>
-      <PriceSlider />
-      <BikesList />
+
+      {loading ? (
+        <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+          <Loader />
+        </div>
+      ) : (
+        <>
+          <PriceSlider />
+          <BikesList />
+        </>
+      )}
     </div>
   );
 }
